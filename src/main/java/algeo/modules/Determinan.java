9ddf34c2353cd7689cec72b87a12a14186231fbf
Hwiyeahh <fira.berlianti@gmail.com>
2025-10-06 14:31:54 +0700

    cek

--- src/main/java/algeo/modules/Determinan.java
+++ src/main/java/algeo/modules/Determinan.java
@@ -1,36 +1,24 @@
 package algeo.modules;
-// import java.util.Scanner;
+import matrix.Matrix;
 
-public class Determinan {
-    public double[][] matrixTranpos(double[][] matrix){
-        double[][] m = new double[matrix.length][matrix.length];
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix.length;j++){
-                m[j][i] = matrix[i][j];
-                System.out.print(m[i][j] + " ");
-            }
-            System.out.println();
-        }
-        return m;
-    }
-    
-    public double detM2x2(double[][] matrix){
-        return (matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0]);
+public class Determinan { 
+    public double detM2x2(Matrix matrix){
+        return (matrix.get(0,0) * matrix.get(1,1)) - (matrix.get(0,1) * matrix.get(1,0));
     }
 
-    public double[][] kofaktor(double[][] matrix, int rows, int cols){
-        double[][] m = new double[matrix.length-1][matrix.length-1];
+    public Matrix kofaktor(Matrix matrix, int rows, int cols){
+        Matrix m = new Matrix(matrix.getRows()-1, matrix.getCols()-1);
         int k=0;
-        for (int i=0;i<matrix.length;i++){
+        for (int i=0;i<matrix.getRows();i++){
             int l=0;
             if (i!=rows){
-                for (int j=0;j<matrix[i].length;j++){
+                for (int j=0;j<matrix.getCols();j++){
                     if (j!=cols){
                         if ((i+j)%2==1){
-                            m[k][l] = -matrix[i][j];
+                            m.set(k, l, -1 * matrix.get(i, j));
                         }
                         else{
-                            m[k][l] = matrix[i][j];
+                            m.set(k, l, matrix.get(i, j));
                         }
                         l++;
                     }
@@ -41,16 +29,16 @@ public class Determinan {
         return m;
     }
 
-    public double detEkspansiKofaktor(double[][] matrix){
-        if (matrix.length == 2){
+    public double detEkspansiKofaktor(Matrix matrix){
+        if (matrix.getRows() == 2){
             return detM2x2(matrix);
         }
         else{
             double hasil=0;
-            for(int j=0;j<matrix.length;j++){
+            for(int j=0;j<matrix.getCols();j++){
                 double ek = detEkspansiKofaktor(kofaktor(matrix,0,j));
-                double tempRes = matrix[0][j] * ek;
-                //System.out.println(tempRes+" = "+matrix[0][j]+" * "+ek);
+                double tempRes = matrix.get(0, j) * ek;
+                //System.out.println(tempRes+" = "+matrix.get(0, j)+" * "+ek);
                 hasil+=tempRes;
                 //System.out.println(hasil);
             }
@@ -58,49 +46,32 @@ public class Determinan {
         }
     }
 
-    public double[][] matrixKofaktor(double[][] matrix){
-        double[][] m = new double[matrix.length][matrix.length];
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix.length;j++){
-                m[i][j] = detEkspansiKofaktor(kofaktor(matrix, i, j));
-                //System.out.print(m[i][j] + " ");
+    public Matrix matrixKofaktor(Matrix matrix){
+        Matrix m = new Matrix(matrix.getRows(), matrix.getCols());
+        for (int i=0;i<matrix.getRows();i++){
+            for (int j=0;j<matrix.getCols();j++){
+                m.set(i, j, detEkspansiKofaktor(kofaktor(matrix, i, j)));
+                //System.out.print(matrix.get(i, j) + " ");
             }
             //System.out.println();
         }
         return m;
     }
 
-    public double[][] adjoin(double[][] matrix){
-        return matrixTranpos(matrixKofaktor(matrix));
-    }
-
-    public double[][] inversAdjoin(double[][] matrix){ 
-        double[][] m = new double[matrix.length][matrix.length];
-        double[][] adj = adjoin(matrix);
-        double det = detEkspansiKofaktor(matrix);
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix.length;j++){
-                m[i][j] = adj[i][j]/det;
-                System.out.print(m[i][j] + " ");
-            }
-            System.out.println();
-        }
-        return m;
-    }
-    public void swapRow(double[][] matrix, int row1, int row2){
+    public void swapRow(Matrix matrix, int row1, int row2){
         if (row1!=row2){
-            for (int j=0;j<matrix.length;j++){
-                double temp =  matrix[row1][j];
-                matrix[row1][j] = matrix[row2][j];
-                matrix[row2][j] = temp;
+            for (int j=0;j<matrix.getCols();j++){
+                double temp = matrix.get(row1, j);
+                matrix.set(row1, j, matrix.get(row2, j));
+                matrix.set(row2, j, temp);
             }
         }
     }
 
-    public int firstZeroTotal(double[] matrixRow){
+    public int firstZeroTotal(Matrix matrix, int row){
         int n=0;
-        for (int j=0;j<matrixRow.length;j++){
-            if (matrixRow[j]!=0){
+        for (int j=0;j<matrix.getCols();j++){
+            if (matrix.get(row, j)!=0){
                 return n;
             }
             else{
@@ -110,45 +81,45 @@ public class Determinan {
         return 0;
     }
 
-    public void multiplyRow(double[] matrixRow, double x){
-        for (int j=0;j<matrixRow.length;j++){
-            matrixRow[j]*=x;
+    public void multiplyRow(Matrix matrix, int row, double x){
+        for (int j=0; j<matrix.getCols(); j++){
+            matrix.set(row, j, matrix.get(row, j) * x);
         }
     }
 
-    public void subtractMultipliedRow(double[] matrixRow1, double[] matrixRow2, double x){
-        for (int j=0;j<matrixRow1.length;j++){
-            matrixRow1[j] -= x*matrixRow2[j];
+    public void subtractMultipliedRow(Matrix matrix, int row1, int row2, double x){
+        for (int j=0; j<matrix.getCols(); j++){
+            matrix.set(row1, j, matrix.get(row1, j) - x * matrix.get(row2, j));
         }
     }
 
-    public double detOBE(double[][] matrix){
-        double[][] m = matrix;
+    public double detOBE(Matrix matrix){
+        Matrix m = matrix.copy();
         int p = 0;
 
         // bikin matrix segitiga
-        for (int i=0;i<m.length;i++){
-            int a = firstZeroTotal(m[i]);
+        for (int i=0;i<m.getRows();i++){
+            int a = firstZeroTotal(m,i);
             System.out.println("firstZero="+a);
             if (a>i){
                 swapRow(m,a,i);
                 System.out.println("swap");
                 p+=1;
             }
-            for (int q=0;q<matrix.length;q++){
-                for (int r=0;r<matrix.length;r++){
-                    System.out.print(m[q][r] + " ");
+            for (int q=0;q<m.getRows();q++){
+                for (int r=0;r<m.getCols();r++){
+                    System.out.print(m.get(q,r) + " ");
                 }
                 System.out.println();
             }
             if (a<i){
                 for (int j=0;j<i;j++){
-                    double x = m[i][j]/m[j][j];
-                    System.out.println(m[i][j] + " / "+m[j][j]+" = "+x);
-                    subtractMultipliedRow(m[i], m[j],x);
-                    for (int s=0;s<matrix.length;s++){
-                        for (int t=0;t<matrix.length;t++){
-                            System.out.print(m[s][t] + " ");
+                    double x = m.get(i, j) / m.get(j, j);
+                    System.out.println(x);
+                    subtractMultipliedRow(m, i, j, x);
+                    for (int q=0;q<m.getRows();q++){
+                        for (int r=0;r<m.getCols();r++){
+                            System.out.print(m.get(q,r) + " ");
                         }
                         System.out.println();
                     }
@@ -164,102 +135,10 @@ public class Determinan {
         else{
             det = -1;
         }
-        for (int i=0;i<matrix.length;i++){
-            det *= matrix[i][i];
+        for (int i=0; i<m.getRows(); i++){
+            det *= m.get(i, i);
         }
         return det;
-    }
-
-    public double[][] inversAugment(double[][] matrix){
-        double[][] m = new double[matrix.length][2*matrix.length];
-        double[][] invers = new double[matrix.length][matrix.length];
-
-        // bikin matrix augment
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<2*matrix.length;j++){
-                if (j<matrix.length){
-                    m[i][j] = matrix[i][j];
-                }
-                else{
-                    if (j-matrix.length==i){
-                        m[i][j] = 1;
-                    }
-                    else{
-                        m[i][j] = 0;
-                    }
-                }
-            }
-        }
-
-        // OBE fase maju
-        for (int i=0;i<m.length;i++){
-            int a = firstZeroTotal(m[i]);
-            System.out.println("firstZero="+a);
-            if (a>i){
-                swapRow(m,a,i);
-                System.out.println("swap");
-            }
-            for (int q=0;q<matrix.length;q++){
-                for (int r=0;r<2*matrix.length;r++){
-                    System.out.print(m[q][r] + " ");
-                }
-                System.out.println();
-            }
-            if (a<i){
-                for (int j=0;j<i;j++){
-                    double x = m[i][j]/m[j][j];
-                    System.out.println(m[i][j] + " / "+m[j][j]+" = "+x);
-                    subtractMultipliedRow(m[i], m[j],x);
-                    for (int s=0;s<matrix.length;s++){
-                        for (int t=0;t<2*matrix.length;t++){
-                            System.out.print(m[s][t] + " ");
-                        }
-                        System.out.println();
-                    }
-                }
-            }
-            if (m[i][i]!=1){
-                double x = 1/m[i][i];
-                multiplyRow(m[i],x);
-            }
-            for (int q=0;q<matrix.length;q++){
-                for (int r=0;r<2*matrix.length;r++){
-                    System.out.print(m[q][r] + " ");
-                }
-                System.out.println();
-            }
-        }
-
-        // OBE fase mundur
-        for (int i=0;i<matrix.length;i++){
-            for(int j=i+1;j<matrix.length;j++){
-                double x = m[i][j]/m[j][j];
-                System.out.println(m[i][j] + " / "+m[j][j]+" = "+x);
-                subtractMultipliedRow(m[i], m[j],x);
-                for (int s=0;s<matrix.length;s++){
-                    for (int t=0;t<2*matrix.length;t++){
-                        System.out.print(m[s][t] + " ");
-                    }
-                    System.out.println();
-                }
-            }
-        }
-
-        // taroh inversnya
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix.length;j++){
-                invers[i][j] = m[i][matrix.length+j];
-            }
-        }
-
-        for (int q=0;q<matrix.length;q++){
-            for (int r=0;r<matrix.length;r++){
-                System.out.print(invers[q][r] + " ");
-            }
-            System.out.println();
-        }
-
-        return m;
     }
 
     // public static void main(String[] args){
--- src/main/java/algeo/modules/Invers.java
+++ src/main/java/algeo/modules/Invers.java
@@ -6,9 +6,9 @@ public class Invers {
         for (int i=0;i<matrix.length;i++){
             for (int j=0;j<matrix.length;j++){
                 m[j][i] = matrix[i][j];
-                //System.out.print(m[i][j] + " ");
+                System.out.print(m[i][j] + " ");
             }
-            //System.out.println();
+            System.out.println();
         }
         return m;
     }
@@ -24,10 +24,103 @@ public class Invers {
         for (int i=0;i<matrix.length;i++){
             for (int j=0;j<matrix.length;j++){
                 m[i][j] = adj[i][j]/det;
-                //System.out.print(m[i][j] + " ");
+                System.out.print(m[i][j] + " ");
             }
-            //System.out.println();
+            System.out.println();
         }
         return m;
     }
+
+    public double[][] inversAugment(double[][] matrix){
+        double[][] m = new double[matrix.length][2*matrix.length];
+        double[][] invers = new double[matrix.length][matrix.length];
+
+        // bikin matrix augment
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<2*matrix.length;j++){
+                if (j<matrix.length){
+                    m[i][j] = matrix[i][j];
+                }
+                else{
+                    if (j-matrix.length==i){
+                        m[i][j] = 1;
+                    }
+                    else{
+                        m[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // OBE fase maju
+        for (int i=0;i<m.length;i++){
+            int a = firstZeroTotal(m[i]);
+            System.out.println("firstZero="+a);
+            if (a>i){
+                swapRow(m,a,i);
+                System.out.println("swap");
+            }
+            for (int q=0;q<matrix.length;q++){
+                for (int r=0;r<2*matrix.length;r++){
+                    System.out.print(m[q][r] + " ");
+                }
+                System.out.println();
+            }
+            if (a<i){
+                for (int j=0;j<i;j++){
+                    double x = m[i][j]/m[j][j];
+                    System.out.println(m[i][j] + " / "+m[j][j]+" = "+x);
+                    subtractMultipliedRow(m[i], m[j],x);
+                    for (int s=0;s<matrix.length;s++){
+                        for (int t=0;t<2*matrix.length;t++){
+                            System.out.print(m[s][t] + " ");
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+            if (m[i][i]!=1){
+                double x = 1/m[i][i];
+                multiplyRow(m[i],x);
+            }
+            for (int q=0;q<matrix.length;q++){
+                for (int r=0;r<2*matrix.length;r++){
+                    System.out.print(m[q][r] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        // OBE fase mundur
+        for (int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix.length;j++){
+                double x = m[i][j]/m[j][j];
+                System.out.println(m[i][j] + " / "+m[j][j]+" = "+x);
+                subtractMultipliedRow(m[i], m[j],x);
+                for (int s=0;s<matrix.length;s++){
+                    for (int t=0;t<2*matrix.length;t++){
+                        System.out.print(m[s][t] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+        // taroh inversnya
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix.length;j++){
+                invers[i][j] = m[i][matrix.length+j];
+            }
+        }
+
+        for (int q=0;q<matrix.length;q++){
+            for (int r=0;r<matrix.length;r++){
+                System.out.print(invers[q][r] + " ");
+            }
+            System.out.println();
+        }
+
+        return m;
+    }
+    
 }
